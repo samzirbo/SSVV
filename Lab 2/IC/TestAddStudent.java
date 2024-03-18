c28568d1bccb669923b7998977ecd63395044e06
samzirbotkw <83039189+samzirbo@users.noreply.github.com>
2024-03-18 09:33:16 +0200

    Lab 2 - IC Documents

---
+++ Lab 2/IC/TestAddStudent.java
@@ -0,0 +1,74 @@
+import domain.Student;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import repository.StudentXMLRepository;
+import service.Service;
+import validation.StudentValidator;
+import validation.ValidationException;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestAddStudent {
+    private StudentXMLRepository repo;
+    private StudentValidator validator;
+    private Service service;
+
+    @BeforeAll
+    static void createXMLFile() {
+        File file = new File("IO/test_add_student.xml");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+                    "<inbox>\n" +
+                    "\n" +
+                    "</inbox>");
+            writer.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    @BeforeEach
+    void setup() {
+        this.validator = new StudentValidator();
+        this.repo = new StudentXMLRepository(validator, "IO/test_add_student.xml");
+        this.service = new Service(this.repo, null, null);
+    }
+
+    @AfterAll
+    static void removeXML() {
+        File file = new File("IO/test_add_student.xml");
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    @Test
+    void testValidation() {
+        int result = service.saveStudent("2", "Maria", 935);
+        assertEquals(1, result);
+        Student student = new Student(null, "George", 935);
+        assertThrows(ValidationException.class, () -> this.validator.validate(student));
+    }
+    
+    @Test
+    void testService() {
+        int result = service.saveStudent("1", "Andrei", 935);
+        assertEquals(1, result);
+        result = service.saveStudent("1", "Sam", 937);
+        assertEquals(0, result);
+
+    }
+
+
+
+}
